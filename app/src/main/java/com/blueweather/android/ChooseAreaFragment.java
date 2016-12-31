c1cc4e643c0690f7fe744c3dd802994bd15f1ae4
QiJunHaoKeLian <272912848@qq.com>
2016-12-31 17:16:34 +0800

    新增切换城市和手动更新天气的功能

--- app/src/main/java/com/blueweather/android/ChooseAreaFragment.java
+++ app/src/main/java/com/blueweather/android/ChooseAreaFragment.java
@@ -121,13 +121,13 @@ public class ChooseAreaFragment extends Fragment {
                         Intent intent = new Intent(getActivity(), WeatherActivity.class);
                         intent.putExtra("weather_id", weatherId);
                         startActivity(intent);
-                        getActivity().finish();}
-//                    } else if (getActivity() instanceof WeatherActivity) {
-//                        WeatherActivity activity = (WeatherActivity) getActivity();
-//                        activity.drawerLayout.closeDrawers();
-//                        activity.swipeRefresh.setRefreshing(true);
-//                        activity.requestWeather(weatherId);
-//                    }
+                        getActivity().finish();
+                    } else if (getActivity() instanceof WeatherActivity) {
+                        WeatherActivity activity = (WeatherActivity) getActivity();
+                        activity.drawerLayout.closeDrawers();
+                        activity.swipeRefresh.setRefreshing(true);
+                        activity.requestWeather(weatherId);
+                    }
                 }
             }
         });
