98d8f3d1f2d1a2123ad3a2a862ee039a3bfc325e
QiJunHaoKeLian <272912848@qq.com>
2016-12-31 11:22:07 +0800

    完成遍历省市县三级列表的功能

---
+++ app/src/main/java/com/blueweather/android/util/HttpUtil.java
@@ -0,0 +1,17 @@
+package com.blueweather.android.util;
+
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+
+/**
+ * Created by admin on 2016/12/31.
+ */
+
+public class HttpUtil {
+    public static void sendOkHttpRequest(String address, okhttp3.Callback callback) {
+        OkHttpClient client = new OkHttpClient();
+        Request request = new Request.Builder().url(address).build();
+        client.newCall(request).enqueue(callback);
+    }
+
+}
